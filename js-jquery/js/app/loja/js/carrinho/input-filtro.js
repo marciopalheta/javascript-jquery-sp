519c7c2a02bacb17d0be4b6521883d9cfadc6bf0
marcio palheta <marcio.palheta@gmail.com>
2014-10-14 13:50:17 -0300

    aula 03

---
+++ js-jquery/js/app/loja/js/carrinho/input-filtro.js
@@ -0,0 +1,9 @@
+var inputs = document.querySelectorAll("#carrinho input");
+for(var i=0; i<inputs.length; i++){
+	inputs[i].addEventListener("input", function(){
+		var regex = /[^0-9]/;
+		if(regex.test(this.value)){
+			this.value = this.value.replace(regex, "");
+		}
+	});
+}
---
+++ js-jquery/js/app/loja/js/carrinho/total.js
@@ -0,0 +1,16 @@
+var inputs = document.querySelectorAll("#carrinho input");
+for (var i=0; i<inputs.length; i++){
+	inputs[i].addEventListener("input", function(event){
+		var input = this;
+		var li = input.parentNode.parentNode.parentNode;
+		var spans = li.querySelectorAll("span");
+		var spanValorUnitario = spans[0];
+		console.log(spanValorUnitario.textContent);
+		var spanValorTotal = spans[1];
+		
+		var quantidade = input.value;
+		var valorUnitario = realParaNumber(spanValorUnitario.textContent);
+		var novoTotal = quantidade * valorUnitario;
+		spanValorTotal.textContent = numberParaReal(novoTotal);
+	});
+}
