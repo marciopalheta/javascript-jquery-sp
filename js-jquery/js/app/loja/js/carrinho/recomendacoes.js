49111ab89552db9eaa4692807b09dc89052cd802
marcio palheta <marcio.palheta@gmail.com>
2014-10-15 12:50:04 -0300

    aula 05

---
+++ js-jquery/js/app/loja/js/carrinho/recomendacoes.js
@@ -0,0 +1,27 @@
+
+
+setInterval(function(){
+	$.getJSON("http://mirrorfashion.caelum.com.br/produtos?callback=?", function(retorno){
+		
+		$(document).trigger("novasRecomendacoes", retorno);
+		
+	});
+}, 5000);
+
+$(document).on("novasRecomendacoes", function(event, retorno){
+	var  painel = $("#recomendacoes");
+	var ul = $("<ul>");
+
+	$.each(retorno.produtos, function(){
+		var li = $("<li>");
+		var img = $("<img>").attr("src", this.imagem);
+		console.log(this.imagem);
+		var pNome = $("<p>").text(this.nome);
+		var pPreco = $("<p>").text(numberParaReal(this.preco));
+	
+		li.append(img).append(pNome).append(pPreco).appendTo(ul);
+	});
+		
+	$("ul", painel).remove();
+	ul.appendTo(painel);
+});
--- js-jquery/js/app/loja/js/sugestoes/sugestoes.js
+++ js-jquery/js/app/loja/js/sugestoes/sugestoes.js
@@ -6,10 +6,22 @@ $("input[type=button]").click(function(event){
 		return false;
 	}
 	
-	var nomeSugestao = $("<span>").addClass("sugestao").text(sugestao);
-	var votos = $("<span>").addClass("votos").text("1 voto");
+	var sugestaoExistente = $(".sugestoes li").filter(function(){
+		console.log(".sugestao: "+$(this).find(".sugestao").text());
+		console.log(sugestao);
+		$(this).removeClass("nova");
+		return $(this).find(".sugestao").text() == sugestao;
+	});
 	
-	$("<li>").append(nomeSugestao).append(votos).appendTo(".sugestoes").addClass("nova");
+	if(sugestaoExistente.length>0){
+		var votos = sugestaoExistente.data("votos")+1;
+		sugestaoExistente.data("votos", votos);
+		sugestaoExistente.find(".votos").text(votos + " votos");
+	}else{
+		var nomeSugestao = $("<span>").addClass("sugestao").text(sugestao);
+		var votos = $("<span>").addClass("votos").text("1 voto");
+		$("<li>").data("votos",1).append(nomeSugestao).append(votos).appendTo(".sugestoes").addClass("nova");	
+	}
 	inputSugestao.val("").focus();
 });
 
